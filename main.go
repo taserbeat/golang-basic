133bf274724894123b0f2bd24ab351ae41a99d5a
taserbeat <taserbeat@gmail.com>
2021-05-26 21:12:58 +0900

    mathパッケージの使い方を追加

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"main/test"
+	"math"
 	"time"
 )
 
@@ -23,4 +24,16 @@ func main() {
 	// 指定した時間を生成
 	specificTime := time.Date(2020, 6, 12, 13, 14, 15, 0, time.Local)
 	fmt.Println(specificTime)
+
+	/* mathパッケージ */
+	// 最大値と最小値
+	maxStr := fmt.Sprintf("%dと%dの最大値: %d", 9, 10, int(math.Max(9, 10)))
+	fmt.Println(maxStr)
+	minStr := fmt.Sprintf("%dと%dの最小値: %d", 1, 2, int(math.Min(1, 2)))
+	fmt.Println(minStr)
+
+	// 小数点以下を切り捨てる
+	fmt.Println(math.Trunc(3.14))  // 3
+	fmt.Println(math.Trunc(-3.14)) // -3
+
 }
